7c3aeec8c8850626d68c9a6dcf5caa600c039cbc
Avanish Srivastava <90971881+Avanishsri31@users.noreply.github.com>
2023-08-15 22:10:52 +0530

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/Avanishsri31/React-Portfolio/main/src/assets/Avanish.pdf`
+const resumeLink = `https://raw.githubusercontent.com/Avanishsri31/React-Portfolio/main/src/assets/Avanish1.pdf`
 
 
 const Resume = () => {
